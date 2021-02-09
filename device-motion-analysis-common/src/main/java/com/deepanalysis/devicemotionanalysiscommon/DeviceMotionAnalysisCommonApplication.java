6a688a3343e16d6b5be0a0dc309b3b428490b3da
Gabriele Raffaelli <g.raffaelli@sintraconsulting.eu>
2021-02-09 19:37:04 +0100

    no message

---
+++ device-motion-analysis-common/src/main/java/com/deepanalysis/devicemotionanalysiscommon/DeviceMotionAnalysisCommonApplication.java
@@ -0,0 +1,13 @@
+package com.deepanalysis.devicemotionanalysiscommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceMotionAnalysisCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceMotionAnalysisCommonApplication.class, args);
+	}
+
+}
---
+++ device-motion-analysis-common/src/test/java/com/deepanalysis/devicemotionanalysiscommon/DeviceMotionAnalysisCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepanalysis.devicemotionanalysiscommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceMotionAnalysisCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
