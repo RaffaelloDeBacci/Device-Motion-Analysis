37d1c3d0a56fd961353ec70767501d46c6b17b64
Gabriele Raffaelli <g.raffaelli@sintraconsulting.eu>
2021-02-09 18:51:11 +0100

    no message

---
+++ src/test/java/com/deepanalysis/motionanalysis/MotionAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepanalysis.motionanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotionAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
