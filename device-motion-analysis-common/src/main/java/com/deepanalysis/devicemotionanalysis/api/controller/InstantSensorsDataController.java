d93c21a2287eee414723863469b8de5e396ea669
Gabriele Raffaelli <g.raffaelli@sintraconsulting.eu>
2021-02-18 22:46:51 +0100

    no message

--- device-motion-analysis-common/src/main/java/com/deepanalysis/devicemotionanalysis/api/controller/InstantSensorsDataController.java
+++ device-motion-analysis-common/src/main/java/com/deepanalysis/devicemotionanalysis/api/controller/InstantSensorsDataController.java
@@ -6,6 +6,9 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.deepanalysis.devicemotionanalysis.common.rest.controller.BaseController;
+import com.deepanalysis.devicemotionanalysis.common.service.InstantSensorsDataService;
+
 @RestController
 @RequestMapping(value = "/api/instant-sensors-data")
 public class InstantSensorsDataController extends BaseController {
