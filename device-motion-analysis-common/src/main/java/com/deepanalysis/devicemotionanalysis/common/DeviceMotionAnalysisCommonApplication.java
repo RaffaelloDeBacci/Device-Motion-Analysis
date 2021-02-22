1104c2ff30e216c78ffb9e2e95497d6fd05f9ed7
Gabriele Raffaelli <g.raffaelli@sintraconsulting.eu>
2021-02-22 20:46:57 +0100

    no message

--- device-motion-analysis-common/src/main/java/com/deepanalysis/devicemotionanalysis/common/DeviceMotionAnalysisCommonApplication.java
+++ device-motion-analysis-common/src/main/java/com/deepanalysis/devicemotionanalysis/common/DeviceMotionAnalysisCommonApplication.java
@@ -2,9 +2,8 @@ package com.deepanalysis.devicemotionanalysis.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(scanBasePackages = "com")
+@SpringBootApplication
 public class DeviceMotionAnalysisCommonApplication {
 
 	public static void main(String[] args) {
